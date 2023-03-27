466bff19879b58e84baff5e766a07e7475154937
Anastasija Mezale <anastasija.mezale@diatomenterprises.com>
2023-03-27 18:21:43 +0300

    #4-15: Adding page title component

--- BethanysPieShopHRM/Pages/EmployeeOverview.razor.cs
+++ BethanysPieShopHRM/Pages/EmployeeOverview.razor.cs
@@ -9,6 +9,8 @@ namespace BethanysPieShopHRM.Pages
 
         private Employee? _selectedEmployee;
 
+        private string Title = "Employee overview";
+
         protected override void OnInitialized()
         {
             Employees = MockDataService.Employees;
