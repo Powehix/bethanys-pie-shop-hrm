f535110bf31a8caf0f6d2e016d2fa0ceeef24be9
Anastasija Mezale <anastasija.mezale@diatomenterprises.com>
2023-03-27 18:15:12 +0300

    #4-13: Adding error boundary for exceptions

--- BethanysPieShopHRM/Components/EmployeeCard.razor.cs
+++ BethanysPieShopHRM/Components/EmployeeCard.razor.cs
@@ -14,6 +14,14 @@ namespace BethanysPieShopHRM.Components
         [Inject]
         public NavigationManager NavigationManager { get; set; } = default!;
 
+        protected override void OnInitialized()
+        {
+            if (string.IsNullOrEmpty(Employee.LastName))
+            {
+                throw new Exception("Last name can't be empty!");
+            }
+        }
+
         public void NavigateToDetails(Employee selectedEmployee)
         {
             NavigationManager.NavigateTo($"/employeedetail/{selectedEmployee.EmployeeId}");
