1a0de9f37c6c8df0fe26ff80bb63f414a20f5f6c
Anastasija Mezale <anastasija.mezale@diatomenterprises.com>
2023-03-28 16:18:41 +0300

    #5-8: Sharing state between components

--- BethanysPieShopHRM.App/Components/Widgets/InboxWidget.razor.cs
+++ BethanysPieShopHRM.App/Components/Widgets/InboxWidget.razor.cs
@@ -1,12 +1,17 @@
-﻿namespace BethanysPieShopHRM.App.Components.Widgets
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.App.Components.Widgets
 {
     public partial class InboxWidget
     {
+        [Inject]
+        public ApplicationState? ApplicationState { get; set; }
+
         public int MessageCount { get; set; } = 0;
 
         protected override void OnInitialized()
         {
-            MessageCount = new Random().Next(10);
+            MessageCount = ApplicationState.NumberOfMessages;
         }
     }
 }
