dbbf3c47b864c83f755fef035f842828e2a6fd3b
Anastasija Mezale <anastasija.mezale@diatomenterprises.com>
2023-03-28 15:49:58 +0300

    #5-6: Adding communication with the API using HttpClient

--- BethanysPieShopHRM.App/Pages/EmployeeOverview.razor.cs
+++ BethanysPieShopHRM.App/Pages/EmployeeOverview.razor.cs
@@ -1,19 +1,23 @@
-﻿using BethanysPieShopHRM.App.Models;
+﻿using BethanysPieShopHRM.App.Services;
 using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
 
 namespace BethanysPieShopHRM.App.Pages
 {
     public partial class EmployeeOverview
     {
+        [Inject]
+        public IEmployeeDataService? EmployeeDataService { get; set; }
+
         public List<Employee>? Employees { get; set; } = default!;
 
         private Employee? _selectedEmployee;
 
         private string Title = "Employee overview";
 
-        protected override void OnInitialized()
+        protected override async Task OnInitializedAsync()
         {
-            Employees = MockDataService.Employees;
+            Employees = (await EmployeeDataService.GetAllEmployees()).ToList();
         }
 
         public void ShowQuickViewPopup(Employee selectedEmployee)
--- BethanysPieShopHRM.App/Program.cs
+++ BethanysPieShopHRM.App/Program.cs
@@ -1,9 +1,12 @@
 using BethanysPieShopHRM.App;
+using BethanysPieShopHRM.App.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient<IEmployeeDataService, EmployeeDataService>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
 await builder.Build().RunAsync();
